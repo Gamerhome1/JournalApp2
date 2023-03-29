6c38e567da9f763f2d24526e3063faaa8d113578
Max <65918596+Busted89@users.noreply.github.com>
2023-03-29 13:42:33 +0300

    Добавьте файлы проекта.

---
+++ JournalApp2/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JournalApp2.ViewModels.Base;
+
+namespace JournalApp2.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string title = "УП наше крутое";
+
+        /// <summary>
+        /// Заголовок окна
+        /// </summary>
+        public string Title
+        {
+            get { return title; }
+            set { Set(ref title, value); }
+        }
+    }
+}
